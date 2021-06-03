39441e248ae6b1a286966aaa2b40c1b70d3e04ec
D <d.koval197@ukr.net>
2021-06-03 13:56:04 +0300

    Some changes

--- check-kafka-topic-create.sh
+++ check-kafka-topic-create.sh
@@ -2,8 +2,9 @@
 
 
 TOPICS_LIST=(test1 test2)
-
+echo "Start"
 echo $KAFKAZKHOSTS
+echo "Stop"
 
 for i in ${TOPICS_LIST[@]}; do
   if /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic $i --zookeeper $KAFKAZKHOSTS; then
