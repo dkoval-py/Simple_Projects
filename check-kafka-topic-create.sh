ae7e198809983b992504936a87461641f748c1cc
D <d.koval197@ukr.net>
2021-06-03 12:33:39 +0300

    Check

--- check-kafka-topic-create.sh
+++ check-kafka-topic-create.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic test-topic --zookeeper zk1-intrac.gvua5lvbodjevd1tlu31gxlc2a.ax.internal.cloudapp.net:2181,zk2-intrac.gvua5lvbodjevd1tlu31gxlc2a.ax.internal.cloudapp.net:2181 > /dev/null 2>&1; then
+        echo "Topic created sucessfully"
         exit 0
 else
-        exit 100
+        echo "Topic already exist"
+        exit 0
 fi
 
