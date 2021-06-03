c1e92a99dd21e0460f41a0fc2a7c0f85f13a8495
D <d.koval197@ukr.net>
2021-06-03 20:37:05 +0300

    output-116.txt

--- check-kafka-topic-create.sh
+++ check-kafka-topic-create.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-wget -O "topics.json" "https://raw.githubusercontent.com/dkoval-py/Simple_Projects/master/topics.json"
-ls -la
-hostname
-ip a
-pwd
+wget -O "/topics.json" "https://raw.githubusercontent.com/dkoval-py/Simple_Projects/master/topics.json"
 KAFKAZKHOSTS=$1
-TOPICS_NAMES=(`jq -c '.[]' topics.json | jq -r '[.name]|join(" ")'`)
-PARTITIONS=(`jq -c '.[]' topics.json | jq -r '[.partitions]|join(" ")'`)
-REPLICATIONS=(`jq -c '.[]' topics.json | jq -r '[.replication]|join(" ")'`)
-echo "Start"
-echo $KAFKAZKHOSTS
-echo $2
-echo $TOPICS_NAMES
-echo $PARTITIONS
-echo $REPLICATIONS
-echo "Stop"
-
+TOPICS_NAMES=(`jq -c '.[]' /topics.json | jq -r '[.name]|join(" ")'`)
+PARTITIONS=(`jq -c '.[]' /topics.json | jq -r '[.partitions]|join(" ")'`)
+REPLICATIONS=(`jq -c '.[]' /topics.json | jq -r '[.replication]|join(" ")'`)
 
 for i in $(seq 1 ${#TOPICS_NAMES[@]}); do
     let "i=i-1"
+    echo $i
+    echo ${TOPICS_NAMES[$i]}
+    echo ${PARTITIONS[$i]}
+    echo ${REPLICATIONS[$i]}
     if /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --replication-factor ${REPLICATIONS[$i]} --partitions ${PARTITIONS[$i]} --topic ${TOPICS_NAMES[$i]} --zookeeper $KAFKAZKHOSTS; then
         echo "Topic created sucessfully"
     else
