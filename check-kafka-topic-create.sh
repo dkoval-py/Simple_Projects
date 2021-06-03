1f07f4e71fd1dfc8d2f375d150fcb6cfa87cafb3
D <d.koval197@ukr.net>
2021-06-03 14:40:45 +0300

    Some changes

--- check-kafka-topic-create.sh
+++ check-kafka-topic-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+KAFKAZKHOSTS=$1
 TOPICS_LIST=(test1 test2)
 echo "Start"
 echo $KAFKAZKHOSTS
@@ -9,9 +9,9 @@ echo "Stop"
 for i in ${TOPICS_LIST[@]}; do
   if /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic $i --zookeeper $KAFKAZKHOSTS; then
         echo "Topic created sucessfully"
-        exit 0
   else
         echo "Topic already exist"
-        exit 0
   fi
 done
+
+exit 0
