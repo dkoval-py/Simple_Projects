89f6929b254e275e9bbd8d3d7e30c61ea8f072b9
D <d.koval197@ukr.net>
2021-06-03 12:24:51 +0300

    Check

--- check-kafka-topic-create.sh
+++ check-kafka-topic-create.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic test-topic --zookeeper zk1-intrac.gvua5lvbodjevd1tlu31gxlc2a.ax.internal.cloudapp.net:2181,zk2-intrac.gvua5lvbodjevd1tlu31gxlc2a.ax.internal.cloudapp.net:2181
+if /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic test-topic --zookeeper zk1-intrac.gvua5lvbodjevd1tlu31gxlc2a.ax.internal.cloudapp.net:2181,zk2-intrac.gvua5lvbodjevd1tlu31gxlc2a.ax.internal.cloudapp.net:2181 > /dev/null 2>&1; then
+        exit 0
+else
+        exit 100
+fi
+
