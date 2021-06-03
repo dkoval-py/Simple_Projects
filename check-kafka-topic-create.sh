716b617d96679e32625b593058d3bf608fa807e2
D <d.koval197@ukr.net>
2021-06-03 20:51:47 +0300

    ${TOPICS_NAMES[$i]}

--- check-kafka-topic-create.sh
+++ check-kafka-topic-create.sh
@@ -8,14 +8,10 @@ REPLICATIONS=(`jq -c '.[]' /topics.json | jq -r '[.replication]|join(" ")'`)
 
 for i in $(seq 1 ${#TOPICS_NAMES[@]}); do
     let "i=i-1"
-    echo $i
-    echo ${TOPICS_NAMES[$i]}
-    echo ${PARTITIONS[$i]}
-    echo ${REPLICATIONS[$i]}
     if /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --replication-factor ${REPLICATIONS[$i]} --partitions ${PARTITIONS[$i]} --topic ${TOPICS_NAMES[$i]} --zookeeper $KAFKAZKHOSTS; then
-        echo "Topic created sucessfully"
+        echo "Topic ${TOPICS_NAMES[$i]} created sucessfully"
     else
-        echo "Topic already exist"
+        echo "Topic ${TOPICS_NAMES[$i]} already exist"
     fi
 done
 
